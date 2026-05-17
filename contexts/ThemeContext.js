fc1c94c5ee9de78a6f105a8f1f34533ea01b4171
ungiosbeem-sketch <ungiosbeem@gmail.com>
2026-05-17 14:55:59 +0300

    Create ThemeContext.js

---
+++ contexts/ThemeContext.js
@@ -0,0 +1,14 @@
+// contexts/ThemeContext.js
+import React, { createContext, useState } from 'react';
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
